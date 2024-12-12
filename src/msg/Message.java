1f48bb68f5b18415ce2a7233b9ce8339f401bbe3
jawadagiv <jawadagi.v@northeastern.edu>
2024-12-12 02:05:49 -0500

    Updated Message class to include creationTime, sentTime and falied flag

--- src/msg/Message.java
+++ src/msg/Message.java
@@ -5,10 +5,39 @@ import java.util.UUID;
 public class Message {
   private String messageId;
   private String content;
+  private final long creationTime;
+  private boolean failed;
+  private long sentTime;
 
   public Message(String content) {
     this.messageId = UUID.randomUUID().toString();
     this.content = content;
+    // Set the time when the message was 'created' in milliseconds
+    this.creationTime = System.currentTimeMillis();
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
+  public long getCreationTime() {
+    return this.creationTime;
+  }
+
+  public boolean isFailed() {
+    return this.failed;
+  }
+
+  public void setFailed(boolean failed) {
+    this.failed = failed;
+  }
+
+  public long getSentTime() {
+    return this.sentTime;
+  }
+
+  public void setSentTime(long sentTime) {
+    this.sentTime = sentTime;
   }
 
   public String toString() {
