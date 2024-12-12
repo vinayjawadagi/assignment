85eb38bc35f304ca0660ee22ab9781668e670390
jawadagiv <jawadagi.v@northeastern.edu>
2024-12-12 00:50:09 -0500

    changed MessageQueue class to include two locks, one for adding into the queue and other for removing from the queue

--- src/msg/MessageQueue.java
+++ src/msg/MessageQueue.java
@@ -6,41 +6,47 @@ import java.util.Queue;
 public class MessageQueue {
   private Queue<Message> queue;
   private int capacity;
+  private Object addLockObject = new Object(); // lock for adding to the queue
+  private Object removeLockObject = new Object(); // lock for removing from the queue
 
   public MessageQueue(int capacity) {
     this.queue = new LinkedList<>();
     this.capacity = capacity;
   }
 
-  public synchronized boolean add(Message item) {
-    // wait until the queue has space for a new message
-    while (queue.size() == capacity) {
-      try {
-        queue.wait();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+  public boolean add(Message item) {
+    synchronized (addLockObject) {
+      // wait until the queue has space for a new message
+      while (queue.size() == capacity) {
+        try {
+          queue.wait();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
       }
+      queue.add(item);
+      // notify all the threads that the lock on queue is released
+      queue.notifyAll();
+      return true;
     }
-    queue.add(item);
-    // notify all the threads that the lock on queue is released
-    queue.notifyAll();
-    return true;
   }
 
   public synchronized Message remove(Message item) {
-    // wait until the queue has messages to be dequeued
-    while (queue.isEmpty()) {
-      try {
-        queue.wait();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+    synchronized (removeLockObject) {
+      // wait until the queue has messages to be dequeued
+      while (queue.isEmpty()) {
+        try {
+          queue.wait();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
       }
-    }
 
-    Message msg = queue.poll();
-    // notify all the threads that the lock on the queue is released
-    queue.notifyAll();
-    return msg;
+      Message msg = queue.poll();
+      // notify all the threads that the lock on the queue is released
+      queue.notifyAll();
+      return msg;
+    }
   }
 
   public synchronized int size() {
