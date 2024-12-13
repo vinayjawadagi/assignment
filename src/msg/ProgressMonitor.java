cf8b62b0991070cb9080567b7a3ab6c5dd32cc68
jawadagiv <jawadagi.v@northeastern.edu>
2024-12-12 19:33:08 -0500

    Updated run() in ProgressMonitor class and added helper method to print stats to console

--- src/msg/ProgressMonitor.java
+++ src/msg/ProgressMonitor.java
@@ -1,4 +1,41 @@
 package msg;
 
-public class ProgressMonitor {
+public class ProgressMonitor implements Runnable {
+    private final MessageStats stats;
+    private final int updateIntervalSeconds;
+    private volatile boolean running = true; // define
+
+    public ProgressMonitor(MessageStats stats, int updateIntervalSeconds) {
+        this.stats = stats;
+        this.updateIntervalSeconds = updateIntervalSeconds;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                // Print current message stats periodically based on update interval
+                printStats();
+                Thread.sleep(updateIntervalSeconds * 1000);
+            } catch (InterruptedException e) {
+                // If Interrupt signal received then stop this thread and set running to false
+                Thread.currentThread().interrupt();
+                running = false;
+            }
+        }
+    }
+
+    // helper method to print stats out to console
+    private void printStats() {
+        System.out.println("\nSMS Simulation Statistics:");
+        System.out.println("Messages Sent: " + stats.getSentCount());
+        System.out.println("Messages Failed: " + stats.getFailedCount());
+        System.out.println("Total Processing Time: " + stats.getTotalProcessingTime());
+        System.out.printf("Average Processing Time: %f ms\n",
+                stats.getAverageProcessingTime());
+    }
+
+    public void stop() {
+        running = false;
+    }
 }
